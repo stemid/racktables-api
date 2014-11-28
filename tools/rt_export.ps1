2d552b38e81735fe1b787eddc8f39ff3ca9c78fa
Stefan Midjich <swehack@gmail.com>
2014-11-28 15:10:24 +0100

    added tools dir with a powershell script to export VMs from vsphere.

---
+++ tools/rt_export.ps1
@@ -0,0 +1,25 @@
+# This exports all VMs from vSphere with information relevant to a Racktables import.
+# by Stefan Midjich <swehack@gmail.com>
+
+add-pssnapin VMware.VimAutomation.Core
+Set-PowerCLIConfiguration -invalidCertificateAction 'ignore' -confirm:$false
+Connect-VIServer -Server 10.220.100.220 -Protocol https
+
+$vms = (get-vm)
+$results = @()
+
+foreach($vm in $vms) {
+  $row = "" | select Folder, Name, HostName, NIC, IP, VLAN, OperatingSystem, Host, Cluster
+  $row.Folder = $vm.Folder
+  $row.Name = $vm.Name
+  $row.HostName = $vm.Guest.HostName
+  $row.OperatingSystem = $vm.Guest.OSFullName
+  $row.NIC = ($vm.Guest.Nics | foreach-object {$_.Device}) -join ','
+  $row.IP = ($vm.Guest | ForEach-Object {$_.IPAddress} | where-object {$_.split('.').length -eq 4}) -join ','
+  $row.VLAN = ($vm | get-networkadapter | foreach-object {$_.NetworkName}) -join ','
+  $row.Host = $vm.VMHost.Name
+  $row.Cluster = (get-cluster -vm $vm)
+  $results += $row
+}
+
+$results | export-csv -path "C:\vSphere scripts\VM CSV export\racktables.csv" -UseCulture -NoTypeInformation
