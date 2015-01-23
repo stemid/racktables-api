a7ba2741be147fa95009a08b70f98b45d68ae166
Stefan Midjich <swehack@gmail.com>
2015-01-23 13:49:06 +0100

    added a new column for adaptertype.

--- tools/rt_export.ps1
+++ tools/rt_export.ps1
@@ -8,16 +8,18 @@ add-pssnapin VMware.VimAutomation.Core
 Set-PowerCLIConfiguration -invalidCertificateAction 'ignore' -confirm:$false
 Connect-VIServer -Server 10.220.100.220 -Protocol https
 
-# This is the tag group with customer names in vSphere.
 $customer_tag = 'Customer Names'
 $vms = (get-vm)
 $results = @()
 
 foreach($vm in $vms) {
-  $row = "" | select Folder, Name, HostName, NIC, IP, VLAN, OperatingSystem, CustomerTag, Tags, Host, Cluster
+  $row = "" | select Folder, Name, HostName, AdapterType, NIC, IP, VLAN, OperatingSystem, CustomerTag, Tags, Host, Cluster
   $row.Folder = $vm.Folder
   $row.Name = $vm.Name
   $row.HostName = $vm.Guest.HostName
+  $row.AdapterType = ($vm | get-networkadapter | foreach-object {
+      $_.Type
+  }) -join ','
   $row.OperatingSystem = $vm.Guest.OSFullName
   $row.NIC = ($vm.Guest.Nics | foreach-object {$_.Device}) -join ','
   $row.IP = ($vm.Guest | ForEach-Object {$_.IPAddress} | where-object {
@@ -32,7 +34,9 @@ foreach($vm in $vms) {
   }) -join ','
   $row.CustomerTag = ($vm | get-tagassignment | foreach-object {
       $_.Tag
-  } | where-object {$_.Category -eq $customer_tag}).Name
+  } | where-object {
+    $_.Category -eq $customer_tag
+  }).Name
   $row.Host = $vm.VMHost.Name
   $row.Cluster = (get-cluster -vm $vm)
   $results += $row
